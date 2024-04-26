334d11e5d1b1969a335221edb09504c69be95a92
Luke Jones <luke@lukeojones.com>
2024-04-26 09:29:48 +0100

    feat(custom) setup eslint, prettier and python formatters

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,12 +1,20 @@
 return {
   'nvimtools/none-ls.nvim',
+  dependencies = {
+    'nvimtools/none-ls-extras.nvim',
+  },
   config = function()
     local null_ls = require 'null-ls'
     null_ls.setup {
       sources = {
+        -- Lua
         null_ls.builtins.formatting.stylua,
-        -- null_ls.builtins.completion.spell,
-        -- require("none-ls.diagnostics.eslint"), -- requires none-ls-extras.nvim
+        -- Javascript
+        null_ls.builtins.formatting.prettier,
+        require 'none-ls.diagnostics.eslint_d',
+        -- Python
+        null_ls.builtins.formatting.isort,
+        null_ls.builtins.formatting.black,
       },
     }
     vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, {})
