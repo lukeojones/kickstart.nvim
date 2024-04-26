732f0ba177bcaed0158224f091847b718b7c0b30
Luke Jones <luke@lukeojones.com>
2024-04-26 09:14:50 +0100

    feat(custom) turn auto-install on for tree-sitter parsers

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,8 @@ return {
   config = function() 
     local config = require("nvim-treesitter.configs")
     config.setup({
-    ensure_installed = { "lua", "vim", "vimdoc", "query", "javascript", "html", "rust", "typescript" },
+    -- ensure_installed = { "lua", "vim", "vimdoc", "query", "javascript", "html", "rust", "typescript" },
+    auto_install = true,
     sync_install = false,
     highlight = { enable = true },
     indent = { enable = true }})
